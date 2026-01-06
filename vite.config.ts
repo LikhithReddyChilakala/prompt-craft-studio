f8a97f45aed976badc5396a074e27f964df1764c
0xLikhith_404 <likhithreddy2423@gmail.com>
2026-01-07 01:22:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // vvvv THIS IS THE IMPORTANT NEW LINE vvvv
-  base: "/Personal_Portfolio/",
+  base: "/portfolio/",
   // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
   server: {
